c0a058f40f104104261c5d5811b00931779af9f1
Collins <jcollins@sdf-login03.slac.stanford.edu>
2022-07-20 11:57:49 -0700

    Added jet_utils

---
+++ utils/jet_utils.py
@@ -0,0 +1,126 @@
+import numpy as np
+from pyjet import cluster
+
+
+def ptetaphiE_to_Epxpypz(jets):
+    pt = jets[:,:,0]
+    eta = jets[:,:,1]
+    phi = jets[:,:,2]
+    E = jets[:,:,3]
+
+    px = pt * np.cos(phi)
+    py = pt * np.sin(phi)
+    pz = pt * np.sinh(eta)
+
+    newjets = np.zeros(jets.shape)
+    newjets[:,:,0] = E
+    newjets[:,:,1] = px
+    newjets[:,:,2] = py
+    newjets[:,:,3] = pz
+
+    return newjets
+
+
+def Epxpypz_to_ptetaphiE(jets):
+
+    E = jets[:,:,0]
+    px = jets[:,:,1]
+    py = jets[:,:,2]
+    pz = jets[:,:,3]
+    
+    pt = np.sqrt(np.square(px) + np.square(py))
+    phi = np.arctan2(py,px)
+    eta = np.arcsinh(pz/pt)
+    
+    newjets = np.zeros(jets.shape)
+    newjets[:,:,0] = pt
+    newjets[:,:,1] = eta
+    newjets[:,:,2] = phi
+    newjets[:,:,3] = E
+    
+    return newjets
+
+
+def ptetaphiE_to_ptyphim(jets):
+    pt = jets[:,:,0]
+    eta = jets[:,:,1]
+    phi = jets[:,:,2]
+    E = jets[:,:,3]
+
+    pz = pt * np.sinh(eta)
+    y = 0.5*np.nan_to_num(np.log((E+pz)/(E-pz)))
+
+    msqr = np.square(E)-np.square(pt)-np.square(pz)
+    msqr[np.abs(msqr) < 1e-6] = 0
+    msqr[msqr < 0.] = 0
+    m = np.sqrt(msqr)
+
+    newjets = np.zeros(jets.shape)
+    newjets[:,:,0] = pt
+    newjets[:,:,1] = y
+    newjets[:,:,2] = phi
+    newjets[:,:,3] = m
+
+    return newjets
+
+
+
+def ptyphim_to_ptetaphiE(jets):
+
+    pt = jets[:,:,0]
+    y = jets[:,:,1]
+    phi = jets[:,:,2]
+    m = jets[:,:,3]
+
+    eta = np.nan_to_num(np.arcsinh(np.sinh(y)*np.sqrt(1+np.square(m/pt))))
+    pz = pt * np.sinh(eta)
+    E = np.sqrt(np.square(pz)+np.square(pt)+np.square(m))
+
+    newjets = np.zeros(jets.shape)
+    newjets[:,:,0] = pt
+    newjets[:,:,1] = eta
+    newjets[:,:,2] = phi
+    newjets[:,:,3] = E
+
+    return newjets
+
+
+
+def center_jets_ptetaphiE(jets):
+    cartesian_jets = ptetaphiE_to_Epxpypz(jets)
+    sumjet_cartesian = np.sum(cartesian_jets,axis=1)
+
+    sumjet_phi = np.arctan2(sumjet_cartesian[:,2],sumjet_cartesian[:,1])
+    sumjet_y = 0.5*np.log((sumjet_cartesian[:,0] + sumjet_cartesian[:,-1])/(sumjet_cartesian[:,0] - sumjet_cartesian[:,-1]))
+
+    ptyphim_jets = ptetaphiE_to_ptyphim(jets)
+    #print(ptyphim_jets[:3,:,:])
+
+    transformed_jets = np.copy(ptyphim_jets)
+    transformed_jets[:,:,1] = ptyphim_jets[:,:,1] - sumjet_y[:,None]
+    transformed_jets[:,:,2] = ptyphim_jets[:,:,2] - sumjet_phi[:,None]
+    transformed_jets[:,:,2] = transformed_jets[:,:,2] + np.pi
+    transformed_jets[:,:,2] = np.mod(transformed_jets[:,:,2],2*np.pi)
+    transformed_jets[:,:,2] = transformed_jets[:,:,2] - np.pi
+
+    transformed_jets[transformed_jets[:,:,0] == 0] = 0
+
+    newjets = ptyphim_to_ptetaphiE(transformed_jets)
+    return newjets
+
+
+def get_clustered_pt_eta_phi(pts, locations,R=0.1):
+    weights = pts
+    outjet = locations
+    myjet = np.zeros((weights.shape[-1]),dtype=([('pT', 'f8'), ('eta', 'f8'), ('phi', 'f8'), ('mass', 'f8')]))
+    myjet['pT'] = weights
+    myjet['eta'] = outjet[:,0]
+    myjet['phi'] = outjet[:,1]
+    sequence = cluster(myjet,R=R,p=0)
+    jets = sequence.inclusive_jets()
+    phis = np.array([np.mod(np.pi+jet.phi,2*np.pi)-np.pi for jet in jets])
+#     phis = [jet.phi for jet in jets]
+    etas = np.array([jet.eta for jet in jets])
+    pts = np.array([jet.pt for jet in jets])
+
+    return pts, etas, phis
